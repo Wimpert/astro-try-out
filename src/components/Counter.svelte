b66035b28b7473aee4a6df25ffcfe0385c567370
Wim Holvoet <holvoetwim@gmail.com>
2022-05-25 13:30:39 +0200

    first commit

---
+++ src/components/Counter.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { count } from "../stores/counter.store";
+
+  let countValue;
+
+  $: countValue = $count;
+</script>
+
+<div class="counter">
+  <button on:click={count.decrement}>-</button>
+  <pre>{countValue}</pre>
+  <button on:click={count.increment}>+</button>
+</div>
+<div class="message">
+  <slot />
+</div>
+
+<style>
+  .counter {
+    display: grid;
+    font-size: 2em;
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+    margin-top: 2em;
+    place-items: center;
+    height: 800px;
+  }
+  .message {
+    text-align: center;
+  }
+</style>
---
+++ src/components/counter-display.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { count } from "../stores/counter.store";
+</script>
+
+<div>
+  <span> count: </span>
+  {$count}
+  <button on:click={count.increment}>+</button>
+</div>
